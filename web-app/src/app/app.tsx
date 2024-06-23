06498e5b00217c4387cbdb7a2938c0805f3299f1
Artur Gubaidullin <lapteuh@gmail.com>
2024-06-23 11:47:21 +0000

    Fix react component

--- web-app/src/app/app.tsx
+++ web-app/src/app/app.tsx
@@ -1,9 +1,12 @@
+import { Chat } from './chat';
 import { UnderDevelopmentAlert } from './under-development-alert';
 
 export function App() {
   return (
     <div className="container mx-auto m-4">
       <UnderDevelopmentAlert />
+
+      <Chat class="mt-4" />
     </div>
   );
 }
