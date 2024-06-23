2cf642891f3f0e8692d87e4aac5d8f2e52365a94
Artur Gubaidullin <lapteuh@gmail.com>
2024-06-23 20:49:19 +0000

    Enable firebase app

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -1,17 +1,21 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './app/app';
+import { FirebaseProgramContext, getFirebaseProgram } from './firebase';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const program = getFirebaseProgram();
+
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <FirebaseProgramContext.Provider value={program}>
+        <App />
+      </FirebaseProgramContext.Provider>
     </BrowserRouter>
   </StrictMode>
 );
