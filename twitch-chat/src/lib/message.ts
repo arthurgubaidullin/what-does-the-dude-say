dcef8347a4ff15db66c9e6ac79d127bca3027cfc
Artur Gubaidullin <lapteuh@gmail.com>
2024-06-23 15:06:38 +0000

    Add function

--- twitch-chat/src/lib/message.ts
+++ twitch-chat/src/lib/message.ts
@@ -1,3 +1,5 @@
+import { NewMessage } from './new-message';
+
 export type Message = Readonly<{
   channel: string;
   message: string;
@@ -5,3 +7,13 @@ export type Message = Readonly<{
   id: string;
   date: Date;
 }>;
+
+export const create = (newMessage: NewMessage): Message => {
+  return {
+    id: crypto.randomUUID(),
+    channel: newMessage.channel,
+    message: newMessage.message,
+    from: newMessage.from,
+    date: new Date(),
+  };
+};
