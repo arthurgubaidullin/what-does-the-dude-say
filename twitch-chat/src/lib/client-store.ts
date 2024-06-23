8af470f8685b75f641234e323a9bc0477fb11992
Artur Gubaidullin <lapteuh@gmail.com>
2024-06-23 20:10:46 +0000

    Fix store

--- twitch-chat/src/lib/client-store.ts
+++ twitch-chat/src/lib/client-store.ts
@@ -4,12 +4,40 @@ import {
   observable,
   onBecomeObserved,
   onBecomeUnobserved,
+  reaction,
 } from 'mobx';
 import { NewMessage } from './new-message';
 import * as Client from './client';
+import { constVoid } from 'fp-ts/function';
 
 export const create = (addNewMessage: (newMessage: NewMessage) => void) => {
-  const client$ = observable.box(Client.create());
+  const client$ = observable.box(Client.create(), { deep: false });
+
+  let unsubscribe = constVoid;
+
+  onBecomeObserved(client$, () => {
+    unsubscribe = reaction(
+      () => client$.get(),
+      (client, previous) => {
+        previous.once('connected', () => {
+          previous.disconnect();
+        });
+
+        client.connect();
+      }
+    );
+  });
+
+  onBecomeUnobserved(client$, () => {
+    unsubscribe();
+    unsubscribe = constVoid;
+  });
+
+  const updateChannels = action((channels: string[]) => {
+    const updated = Client.create(channels);
+
+    client$.set(updated);
+  });
 
   autorun(() => {
     client$.get().on('message', (channel, user, message) => {
@@ -19,26 +47,6 @@ export const create = (addNewMessage: (newMessage: NewMessage) => void) => {
         from: user.username ?? 'unknown',
       });
     });
-  });
-
-  let seq = Promise.resolve();
-
-  onBecomeObserved(client$, () => {
-    seq = seq.then(async () => {
-      await client$.get().connect();
-    });
-  });
-
-  onBecomeUnobserved(client$, () => {
-    seq = seq.then(async () => {
-      await client$.get().disconnect();
-    });
-  });
-
-  const updateChannels = action((channels: string[]) => {
-    const updated = Client.create(channels);
-
-    client$.set(updated);
   });
 
   return {
