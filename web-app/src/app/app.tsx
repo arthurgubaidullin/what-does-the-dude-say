3ab47278a4a4a17f14acfeffca04b00d0acf248a
Artur Gubaidullin <lapteuh@gmail.com>
2024-06-23 11:30:36 +0000

    Fix react component

--- web-app/src/app/app.tsx
+++ web-app/src/app/app.tsx
@@ -1,5 +1,11 @@
+import { UnderDevelopmentAlert } from './under-development-alert';
+
 export function App() {
-  return <div>test</div>;
+  return (
+    <div className="container mx-auto m-4">
+      <UnderDevelopmentAlert />
+    </div>
+  );
 }
 
 export default App;
