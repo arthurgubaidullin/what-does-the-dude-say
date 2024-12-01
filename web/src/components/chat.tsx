386098795d28493a8422bde81e188e909c8785fa
Artur Gubaidullin <lapteuh@gmail.com>
2024-12-01 15:30:10 +0000

    Update component

--- web/src/components/chat.tsx
+++ web/src/components/chat.tsx
@@ -1,16 +1,22 @@
 import { useState } from 'preact/hooks';
-import { type ChatService } from '../services/chat';
-import { InMemoryChatService } from '../services/in-memory-chat';
+import { type Program } from '../services/program';
+import { OnOffSpeecher } from './on-off-speecher';
+import { InMemoryProgramFactory } from '../services/in-memory-program-factory';
 
 export const Chat = () => {
-  const [chatService] = useState<ChatService>(() => new InMemoryChatService());
+  const [program] = useState<Program>(() => InMemoryProgramFactory.get());
+
+  const speecher = program.speecher;
 
   return (
     <section class="grid gap-4">
-      <h2 class="text-5xl">Chat messages</h2>
+      <div class="flex gap-1">
+        <h2 class="text-5xl">Chat messages</h2>
+        <OnOffSpeecher speecher={speecher} />
+      </div>
 
       <div>
-        {chatService.messages.value.map((message) => (
+        {program.chat.messages.value.map((message) => (
           <div class="chat chat-start">
             <div class="chat-header">{message.from}</div>
             <div class="chat-bubble">{message.content}</div>
