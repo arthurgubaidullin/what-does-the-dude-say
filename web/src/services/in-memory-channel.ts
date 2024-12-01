9c274c6b7e57d4425691ea4b47bc3fbdb34487c1
Artur Gubaidullin <lapteuh@gmail.com>
2024-12-01 16:53:52 +0000

    Fix in-memory channel service

--- web/src/services/in-memory-channel.ts
+++ web/src/services/in-memory-channel.ts
@@ -4,11 +4,16 @@ import type { ChannelService } from './channel';
 export class InMemoryChannelService implements ChannelService {
   #channel: Signal<string | undefined> = signal(undefined);
 
+  constructor() {}
+
   get name(): ReadonlySignal<string | undefined> {
     return this.#channel;
   }
 
   select(this: this, channel: string) {
+    if (channel === this.#channel.peek()) {
+      return;
+    }
     if (channel.length === 0) {
       this.#channel.value = undefined;
       return;
