56df6d267da16570c6d38618d12744c9d5ddc672
Artur Gubaidullin <lapteuh@gmail.com>
2024-12-01 16:25:31 +0000

    Add twitch service

---
+++ web/src/services/twitch.ts
@@ -0,0 +1,11 @@
+import { type ReadonlySignal } from '@preact/signals';
+
+export interface Message {
+  readonly id: string;
+  readonly from: string;
+  readonly content: string;
+}
+
+export interface TwitchService {
+  readonly lastMessage: ReadonlySignal<Message | undefined>;
+}
