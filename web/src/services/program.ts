43911f6aa63bc9d1e4ea4ae4253757456968a12c
Artur Gubaidullin <lapteuh@gmail.com>
2024-12-01 15:29:29 +0000

    Add program type

---
+++ web/src/services/program.ts
@@ -0,0 +1,7 @@
+import type { ChatService } from './chat';
+import { type SpeecherService } from './speecher';
+
+export interface Program {
+  readonly chat: ChatService;
+  readonly speecher: SpeecherService;
+}
