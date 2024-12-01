6dc56747da82f68712146db903f4ae3d680ec843
Artur Gubaidullin <lapteuh@gmail.com>
2024-12-01 18:19:28 +0000

    Add channel selection

--- web/src/components/chat.tsx
+++ web/src/components/chat.tsx
@@ -1,22 +1,21 @@
-import { useState } from 'preact/hooks';
-import { type Program } from '../services/program';
+import { program } from '../program-instance';
+import { ChangeChannelForm } from './change-channel-form';
 import { OnOffSpeecher } from './on-off-speecher';
-import { InMemoryProgramFactory } from '../services/in-memory-program-factory';
 
 export const Chat = () => {
-  const [program] = useState<Program>(() => InMemoryProgramFactory.get());
-
-  const speecher = program.speecher;
+  const { speecher, chat, channel } = program;
 
   return (
     <section class="grid gap-4">
+      <ChangeChannelForm channel={channel} />
+
       <div class="flex gap-1">
         <h2 class="text-5xl">Chat messages</h2>
         <OnOffSpeecher speecher={speecher} />
       </div>
 
       <div>
-        {program.chat.messages.value.map((message) => (
+        {chat.messages.value.map((message) => (
           <div class="chat chat-start">
             <div class="chat-header">{message.from}</div>
             <div class="chat-bubble">{message.content}</div>
